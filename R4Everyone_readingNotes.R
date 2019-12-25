5748101fe2f7ed5857b38b7fd7b81287e9509686
SmilodonCub <smilodon138@gmail.com>
2019-12-25 17:23:10 -0500

    chapter7

--- R4Everyone_readingNotes.R
+++ R4Everyone_readingNotes.R
@@ -781,3 +781,48 @@ map2( firstList, secondList, simpleFunc )
 map2_int( firstList, secondList, simpleFunc )
 pmap( list( firstList, secondList), simpleFunc )
 pmap_int( list( firstList, secondList ), simpleFunc )
+
+
+#Statistical Graphics
+library( ggplot2 )
+data( diamonds )
+head( diamonds )
+hist( diamonds$carat, main="Carat Histogram", xlab="Carat" )
+plot(price ~ carat, data=diamonds)
+plot( diamonds$carat, diamonds$price )
+boxplot( diamonds$carat )
+#ggplot2
+ggplot( data=diamonds ) + geom_histogram( aes( x = carat ) )
+ggplot( data=diamonds ) + geom_density( aes(x=carat), fill = 'grey50' )
+ggplot( diamonds, aes(x=carat,y=price)) + geom_point()
+g <- ggplot( diamonds, aes(x=carat,y=price))
+g + geom_point( aes( color=color ) )
+g + geom_point(aes(color=color)) + facet_wrap(~color)
+g + geom_point(aes(color=color)) + facet_grid(cut~clarity)
+ggplot(diamonds, aes(x=carat)) + geom_histogram() + facet_wrap(~color)
+#boxplots
+ggplot(diamonds, aes(y=carat, x=cut)) + geom_boxplot()
+ggplot(diamonds, aes(y=carat, x=cut)) + geom_violin()
+ggplot(diamonds, aes(y=carat, x=cut)) + geom_point() + geom_violin()
+ggplot(diamonds, aes(y=carat, x=cut)) + geom_violin() + geom_point()
+#lineplots
+ggplot(economics, aes(x=date, y=pop)) + geom_line()
+library( lubridate )
+economics$year <- year(economics$date)
+economics$month  <- month(economics$date, label=TRUE)
+econ2000 <- economics[which(economics$year >= 2000), ]#which is kinda like SQL WHERE
+library( scales )
+g <- ggplot(econ2000, aes(x=month, y=pop))
+g <- g + geom_line(aes(color=factor(year), group=year))#year is converted to a factor so that we can get a different color
+g <- g + scale_y_continuous(labels=comma)
+g <- g + labs(title="Population Growth", x="Month", y="Population")
+g
+#Themes
+library( ggthemes )
+g2 <- ggplot(diamonds, aes(x=carat, y=price)) + geom_point(aes(color=color))
+g2 + theme_economist() + scale_colour_economist()
+g2
+g2 + theme_excel() + scale_colour_excel()
+g2 + theme_tufte()
+g2 + theme_wsj()
+
